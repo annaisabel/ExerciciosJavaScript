496dab9785fdb261cd35bf25156b242e29b10c1b
annaisabel <annaisabelloureiro@gmail.com>
2022-05-31 19:34:56 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,8 @@
+const nome = "Anna Isabel";
+const idade = 25;
+let turmaminas = "2022.1";
+let verdadeiro = true;
+let falso = false;
+
+console.log(`Meu nome é ${nome}, eu tenho ${idade} anos, sou da turma ${turmaminas} do curso de FrontEnd do MinasProgramam.`);
+console.log( `Gosto de viajar: ${verdadeiro}`)
